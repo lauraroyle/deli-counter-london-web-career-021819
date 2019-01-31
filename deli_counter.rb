6147b8440f4d95fc26fc6f33312f69eca656152f
Laura Royle <lauraamie.royle@gmail.com>
2019-01-31 13:52:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,16 @@
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
+    if katz_deli.length == 0
+        puts "The line is currently empty."
 
-  else
-    message = "The line is currently:" #first half of the message
-    katz_deli.each_with_index do |name, index| #name is placeholder local varable for method to use. Index is the indox of the array
-      message += " #{index.to_i+1}. #{name}" # index to integer will give you the array index (+1), plus name of person
-  end
-  puts "#{message}" #puts the message variable
-  end
+    else
+        message = "The line is currently:" #first half of the message
+          katz_deli.each_with_index do |name, index| #name is placeholder local varable for method to use. Index is the indox of the array
+            message += " #{index.to_i+1}. #{name}" # index to integer will give you the array index (+1), plus name of person
+            end
+              puts "#{message}" #puts the message variable
+    end
+end
+
+def take_a_number
 
 end
