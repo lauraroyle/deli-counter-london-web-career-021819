773b0f257c12a3d4033764878425aa873e04d34e
Laura Royle <lauraamie.royle@gmail.com>
2019-01-31 13:45:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,12 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
 
-  else katz_deli.each_with_index do |name, index|
-    
+  else 
+    message = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      message += " #{index.to_i+1}. #{value}"
   end
-
+  puts "#{message}"
   end
 
 end
